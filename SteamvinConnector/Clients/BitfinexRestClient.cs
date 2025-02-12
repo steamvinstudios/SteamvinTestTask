39f791e05ab0f9b8b7d362ebcc4e976973ac1236
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2025-02-12 17:14:43 +0300

    Реализация REST клиента для Bitfinex

--- SteamvinConnector/Clients/BitfinexRestClient.cs
+++ SteamvinConnector/Clients/BitfinexRestClient.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BitfinexConnector.Core.Interfaces;
+using BitfinexConnector.Core.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace BitfinexConnector.Core.Clients
 {
-    class BitfinexRestClient
+    class BitfinexRestClient : IRestClient
     {
+        private readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://api.bitfinex.com/v2/")
+        };
+
+        public async Task<IEnumerable<Trade>> GetTradesAsync(string symbol, DateTime start, DateTime end)
+        {
+            var endpoint = $"trades/{symbol}/hist?start={ToUnixMs(start)}&end={ToUnixMs(end)}";
+            var response = await _httpClient.GetStringAsync(endpoint);
+            return JsonConvert.DeserializeObject<List<Trade>>(response);
+        }
+
+        private static long ToUnixMs(DateTime date) =>
+            new DateTimeOffset(date).ToUnixTimeMilliseconds();
+
+        public Task<IEnumerable<Candle>> GetCandlesAsync(string symbol, string timeframe, DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Ticker> GetTickerAsync(string symbol)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
