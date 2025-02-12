636dd4d7e610858cc3aea458d20ff511e551181b
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2025-02-12 17:12:13 +0300

    Интерфейсы для клиентов

--- SteamvinConnector/Interfaces/IRestClient.cs
+++ SteamvinConnector/Interfaces/IRestClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BitfinexConnector.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace BitfinexConnector.Core.Interfaces
 {
     interface IRestClient
     {
+        Task<IEnumerable<Trade>> GetTradesAsync(string symbol, DateTime start, DateTime end);
+        Task<IEnumerable<Candle>> GetCandlesAsync(string symbol, string timeframe, DateTime start, DateTime end);
+        Task<Ticker> GetTickerAsync(string symbol);
     }
 }
